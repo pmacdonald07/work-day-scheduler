d0a5378b9936240d9877221f059810addc155833
Patrick M <patrickmacdonald07@gmail.com>
2022-08-30 18:02:17 -0400

    save and load tasks from local storage ready

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,19 +18,11 @@ $("#fourPM").text(moment().hours(16).format("hA"));
 
 $("#fivePM").text(moment().hours(17).format("hA"));
 
-hoursArray = [
-    9, 10, 11, 12, 13, 14, 15, 16, 17
-];
+taskArray = [];
 
-//var currentTime = moment().format("hA");
+bodyEl = $("body");
 
-//console.log(currentTime);
-
-/*if (currentTime > tenAMTest) {
-    var parentDiv = $("#tenAM").closest(".form-group").children(".timeSlot").css("background-color", "red");
-    //parentDiv.css("background-color", "red");
-    
-}*/
+let formArray = [$("#rowNine"), $("#rowTen"), $("#rowEleven"), $("#rowTwelve"), $("#rowOne"), $("#rowTwo"), $("#rowThree"), $("rowFour"), $("#rowFive")]
 
 function auditTime() {
     $("div.form-group").each(function(){
@@ -42,19 +34,6 @@ function auditTime() {
         currentTime = moment(currentTime, "hA");
         timeTest = moment(timeTest, "hA");
 
-        console.log(timeTest);
-        //console.log(moment(timeTest, "DD.MM.YYYY HH:mm"))
-        console.log(currentTime.isAfter(timeTest));
-        console.log(currentTime.isBefore(timeTest));
-        console.log(currentTime.isSame(timeTest));
-        console.log(currentTime < timeTest);
-        console.log(currentTime > timeTest);
-        console.log(currentTime == timeTest);
-        //console.log(currentTime.diff(timeTest, "hours"));
-
-        //currentTime = moment(currentTime);
-        //console.log(moment().isBefore(timeTest));
-        //debugger;
         if (currentTime.isAfter(timeTest)) {
             inputClass.removeClass("bg-danger bg-success");
             inputClass.addClass("bg-secondary");
@@ -79,10 +58,50 @@ function auditTime() {
 
     })
 }
+
+function taskSubmit (event) {
+    event.preventDefault();
+    var inputVal = $(this).find("input").val();
+    console.log(inputVal);
+    console.log(event.target);
+    
+    var task = {
+        timeID: $(this).find("label").attr("id"),
+        taskInfo: inputVal
+    };
+
+    console.log(task);
+    taskArray.push(task);
+    console.log(taskArray);
+
+    saveTasks();
+}
+
+function saveTasks () {
+    localStorage.setItem("tasks", JSON.stringify(taskArray));
+}
+
+function loadTasks () {
+    tasksLocal = localStorage.getItem("tasks");
+    tasksLocal = JSON.parse(tasksLocal);
+    taskArray = tasksLocal;
+
+    if (taskArray === null) {
+        taskArray = [];
+    }
+
+    for (i = 0; i < tasksLocal.length; i++) {
+        $("label").each(function() {
+            if (tasksLocal[i].timeID === $(this).attr("id")) {
+                console.log($(this).parent(".form-group"));
+                $(this).parent(".form-group").find("input").val(tasksLocal[i].taskInfo);
+                console.log(tasksLocal[i].taskInfo);
+            }
+        })
+    }
+}
+
+$(".formRow").on("submit", taskSubmit);
+
 auditTime();
-/*var currentTimeString = currentTime.toString();
-console.log(currentTimeString);
-console.log(currentTime);
-var timeSlice = currentTimeString.slice(0, 1);
-var timeNumber = parseInt(timeSlice);
-console.log(timeNumber); */
+loadTasks();
